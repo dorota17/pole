e5a7eacede9a0cb8480efbfbdb43a6cc693afb0f
dorota17 <100830633+dorota17@users.noreply.github.com>
2022-03-03 08:25:29 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define pi 3.14
+int main(void) {
+  int r=10;
+  double pole=pi*r*r;
+    
+  printf("Pole jest równe: %f\n",pole);
+  return 0;
+}
+/* funkcja scan i przetestować wprowadzanie danych do programu, zrobić 3 pierwsze zadania */
